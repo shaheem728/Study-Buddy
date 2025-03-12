4eda659694ff5f34119b8156a606eb75f65d135f
shaheem <shaheemmahmood728@gmail.com>
2025-03-12 10:04:03 +0530

    Added login and register functionality with user authentication and authorization

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/',views.loginPage,name='login'),
+    path('register/',views.registerPage,name='register'),
     path('logout/',views.logoutUser,name='logout'),
     path('room/<str:pk>/',views.room,name="room"),
     path('create-room/', views.createRoom, name="create-room"),
--- base/views.py
+++ base/views.py
@@ -4,15 +4,17 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
-from .models import Room,Topic
+from .models import Room,Topic,Message
 from .forms import RoomForm
 # Create your views here.
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         user = authenticate(request,username=username,password=password)    
         if user is not None:
@@ -20,10 +22,27 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Invalid username or password')
+    context = {'page':page}    
     return render(request,'base/login_register.html',context)
 def logoutUser(request):
     logout(request)
     return redirect('home')
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+           user = form.save(commit=False)
+           user.username = user.username.lower()
+           user.save()
+           login(request,user)
+           return redirect('home')
+        else:
+            messages.error(request, 'An error accurred during registration')
+    context = {'page':page,'form':form}
+    return render(request,'base/login_register.html',context)
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.filter(Q(topic__name__icontains = q)|
@@ -36,7 +55,15 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    room_messages = room.message_set.all().order_by('-created')  #----------------
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+            )
+        return redirect('room', pk=room.id)
+    context = {'room':room,'room_messages':room_messages}
     return render(request,'base/room.html',context)
 
 @login_required(login_url='login')
